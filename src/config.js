99591b351ac0da1dfef1897ad39686734b1d2ad2
Abdykali Azat <azat.abdykalli@gmail.com>
2024-08-09 11:50:50 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://whitekargo.kz:3001',
+    
+    nameCargo: 'White Kargo'
+  };
+  
+  
+export default config;
+  
